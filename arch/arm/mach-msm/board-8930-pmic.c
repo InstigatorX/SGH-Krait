b33b7f1f092352647fdd2173db71ff34c22d8f19
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2012-08-01 21:33:09 -0700

    power: pm8921-bms: limit voltage correction
    The "adjust_soc" algorithm where we change the open circuit voltage
    (ocv) in steps so that the state of charge (soc) starts approaching
    the estimated soc, causes nonlinearity in the soc curves if an incorrect
    resistance value is fed to the algorithm.
    
    As battery ages and temperature changes, it is hard to estimate the
    exact battery resistance.
    
    So to fix the nonlinearity, limit the amount by which the ocv is
    changed. Make it proportional to the current, i.e. change ocv by
    small amounts in light load and let it change by proportionally
    large amounts in heavy load situations.
    
    Also, make the point where the soc is adjusted configurable via platform
    data instead of forcing it to 25%.
    
    Change-Id: Idc141e6bf3172dab278afe1900f5a1f9cdd624dd
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>

--- arch/arm/mach-msm/board-8930-pmic.c
+++ arch/arm/mach-msm/board-8930-pmic.c
@@ -348,6 +348,7 @@ static struct pm8921_bms_platform_data pm8921_bms_pdata __devinitdata = {
 	.v_cutoff		= 3400,
 	.max_voltage_uv		= MAX_VOLTAGE_MV * 1000,
 	.shutdown_soc_valid_limit = 20,
+	.adjust_soc_low_threshold = 25,
 };
 
 >>>>>>> 3c37882... power: bms/ccadc: move ccadc calibration to ccadc driver
