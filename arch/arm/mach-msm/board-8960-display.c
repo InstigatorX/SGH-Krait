ee683ae89154a7ac994d0ac7284e1d968e2fe644
Steve Kondik <shade@chemlab.org>
2012-12-02 06:30:16 -0800

    msm: Fix MDP bus scaling data
    Change-Id: I958cc19ffbdded2a763221b9b41ca3be5f70f7c0

--- arch/arm/mach-msm/board-8960-display.c
+++ arch/arm/mach-msm/board-8960-display.c
@@ -1407,8 +1407,8 @@ static struct msm_bus_vectors mdp_ui_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_MDP_PORT0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
-		.ab = 248832000 * 2,
-		.ib = 311040000 * 2,
+		.ab = 216000000 * 2,
+		.ib = 270000000 * 2,
 	},
 };
 
@@ -1417,8 +1417,8 @@ static struct msm_bus_vectors mdp_vga_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_MDP_PORT0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
-		.ab = 248832000 * 2,
-		.ib = 311040000 * 2,
+		.ab = 216000000 * 2,
+		.ib = 270000000 * 2,
 	},
 };
 
@@ -1427,8 +1427,8 @@ static struct msm_bus_vectors mdp_720p_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_MDP_PORT0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
-		.ab = 304128000 * 2,
-		.ib = 380160000 * 2,
+		.ab = 230400000 * 2,
+		.ib = 288000000 * 2,
 	},
 };
 
@@ -1437,8 +1437,8 @@ static struct msm_bus_vectors mdp_1080p_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_MDP_PORT0,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
-		.ab = 407808000 * 4,
-		.ib = 509760000 * 4,
+		.ab = 334080000 * 2,
+		.ib = 417600000 * 2,
 	},
 };
 
