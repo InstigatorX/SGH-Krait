30312187f1ef2ea6d0e6802e4d93438c46198e92
InstigatorX <sloebric@gmail.com>
2012-12-13 11:09:41 -0800

    BUILD: Script updates

--- build-stock.sh
+++ build-stock.sh
@@ -5,19 +5,19 @@ export LOCALVERSION="$timestamp"
 
 make -j8
 
-cp arch/arm/mach-msm/dma_test.ko ../packaging/Bootv2/system/lib/modules
-cp arch/arm/mach-msm/msm-buspm-dev.ko ../packaging/Bootv2/system/lib/modules
-cp arch/arm/mach-msm/reset_modem.ko ../packaging/Bootv2/system/lib/modules
-cp crypto/ansi_cprng.ko ../packaging/Bootv2/system/lib/modules
-cp drivers/crypto/msm/qce40.ko ../packaging/Bootv2/system/lib/modules
-cp drivers/crypto/msm/qcedev.ko ../packaging/Bootv2/system/lib/modules
-cp drivers/crypto/msm/qcrypto.ko ../packaging/Bootv2/system/lib/modules
-cp drivers/input/evbug.ko ../packaging/Bootv2/system/lib/modules
-cp drivers/media/video/gspca/gspca_main.ko ../packaging/Bootv2/system/lib/modules
-cp drivers/net/wireless/bcmdhd/dhd.ko ../packaging/Bootv2/system/lib/modules
-cp drivers/net/wireless/btlock/btlock.ko ../packaging/Bootv2/system/lib/modules
-cp drivers/scsi/scsi_wait_scan.ko ../packaging/Bootv2/system/lib/modules
-cp drivers/spi/spidev.ko ../packaging/Bootv2/system/lib/modules
+cp arch/arm/mach-msm/dma_test.ko build_dir/system/lib/modules
+cp arch/arm/mach-msm/msm-buspm-dev.ko build_dir/system/lib/modules
+cp arch/arm/mach-msm/reset_modem.ko build_dir/system/lib/modules
+cp crypto/ansi_cprng.ko build_dir/system/lib/modules
+cp drivers/crypto/msm/qce40.ko build_dir/system/lib/modules
+cp drivers/crypto/msm/qcedev.ko build_dir/system/lib/modules
+cp drivers/crypto/msm/qcrypto.ko build_dir/system/lib/modules
+cp drivers/input/evbug.ko build_dir/system/lib/modules
+cp drivers/media/video/gspca/gspca_main.ko build_dir/system/lib/modules
+cp drivers/net/wireless/bcmdhd/dhd.ko build_dir/system/lib/modules
+cp drivers/net/wireless/btlock/btlock.ko build_dir/system/lib/modules
+cp drivers/scsi/scsi_wait_scan.ko build_dir/system/lib/modules
+cp drivers/spi/spidev.ko build_dir/system/lib/modules
 
 ../packaging/mkbootfs ramdisk/initramfs | gzip > ramdisk/ramdisk.gz
 ../packaging/mkbootimg --kernel arch/arm/boot/zImage --ramdisk ramdisk/ramdisk.gz --cmdline "androidboot.hardware=qcom user_debug=31" -o build_dir/boot.img --base 0x80200000 --pagesize 2048 --ramdiskaddr 0x81500000
