1bdbd36baacb9a667ba7ba67d08c2db102fa66be
InstigatorX <sloebric@gmail.com>
2012-12-13 11:08:12 -0800

    Revert "Disable mDNS completely due to excessive wakelock: from madmack"
    This reverts commit 175bb4a524867cd6bef819550bdf57c8421c40bd.

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -3920,7 +3920,8 @@ dhd_preinit_ioctls(dhd_pub_t *dhd)
 	dhd->pktfilter[1] = NULL;
 	dhd->pktfilter[2] = NULL;
 	dhd->pktfilter[3] = NULL;
-	dhd->pktfilter[4] = NULL;
+	/* Add filter to pass multicastDNS packet and NOT filter out as Broadcast */
+	dhd->pktfilter[4] = "104 0 0 0 0xFFFFFFFFFFFF 0x01005E0000FB";
 
 #if defined(SOFTAP)
 	if (ap_fw_loaded) {
@@ -5149,8 +5150,6 @@ int net_os_rxfilter_add_remove(struct net_device *dev, int add_remove, int num)
 			filterp = "101 0 0 0 0xFFFFFFFFFFFF 0xFFFFFFFFFFFF";
 			break;
 		case DHD_MULTICAST4_FILTER_NUM:
-			//Disable this userland filter
-			return ret;
 			filterp = "102 0 0 0 0xFFFFFF 0x01005E";
 			break;
 		case DHD_MULTICAST6_FILTER_NUM:
