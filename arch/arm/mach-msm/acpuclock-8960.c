6507a8f84163d843195363521206aba6996932c6
InstigatorX <sloebric@gmail.com>
2013-01-11 13:50:48 -0800

    OC: Limit speed to 1.512GHz

--- arch/arm/mach-msm/acpuclock-8960.c
+++ arch/arm/mach-msm/acpuclock-8960.c
@@ -295,10 +295,10 @@ static struct acpu_level acpu_freq_tbl_8960_kraitv2_slow[] = {
 	{ 1, {  1458000, HFPLL, 1, 0, 0x36 }, L2(19), 1237500 },
 	{ 1, {  1512000, HFPLL, 1, 0, 0x38 }, L2(19), 1250000 },
 #ifdef CONFIG_CPU_OVERCLOCK
-	{ 1, {  1620000, HFPLL, 1, 0, 0x3C }, L2(19), 1275000 },
+	{ 1, {  1620000, HFPLL, 1, 0, 0x3C }, L2(19), 1300000 },
 #ifdef CONFIG_CPU_ULTIMATE
-	{ 1, {  1728000, HFPLL, 1, 0, 0x40 }, L2(19), 1325000 },
-	{ 1, {  1836000, HFPLL, 1, 0, 0x44 }, L2(19), 1350000 },
+	{ 1, {  1728000, HFPLL, 1, 0, 0x40 }, L2(19), 1350000 },
+	{ 1, {  1836000, HFPLL, 1, 0, 0x44 }, L2(19), 1400000 },
 #endif
 #endif
 	{ 0, { 0 } }
@@ -339,6 +339,8 @@ static struct acpu_level acpu_freq_tbl_8960_kraitv2_nom[] = {
 	{ 1, {  1620000, HFPLL, 1, 0, 0x3C }, L2(19), 1250000 },
 #ifdef CONFIG_CPU_ULTIMATE
 	{ 1, {  1728000, HFPLL, 1, 0, 0x40 }, L2(19), 1350000 },
+	{ 1, {  1836000, HFPLL, 1, 0, 0x44 }, L2(19), 1400000 },
+	{ 1, {  1890000, HFPLL, 1, 0, 0x46 }, L2(19), 1400000 },
 #endif
 #endif
 	{ 0, { 0 } }
@@ -1244,9 +1246,11 @@ static struct acpu_level * __init select_freq_plan(void)
 		kraitv2_apply_vmin(acpu_freq_tbl);
 
 	/* Find the max supported scaling frequency. */
-	for (l = acpu_freq_tbl; l->speed.khz != 0; l++)
+	for (l = acpu_freq_tbl; l->speed.khz != 0; l++) {
 		if (l->use_for_scaling)
 			max_acpu_level = l;
+		if (l->speed.khz > 1458000) break;
+	}
 	BUG_ON(!max_acpu_level);
 	pr_info("Max ACPU freq: %u KHz\n", max_acpu_level->speed.khz);
 
