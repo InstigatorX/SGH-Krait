3b6184d22a34d22b76c34b24d99ce230366cd2eb
InstigatorX <sloebric@gmail.com>
2013-01-18 16:44:52 -0800

    BUILD: Remove vpn and added RAM_CONSOLE

--- build-stock.sh
+++ build-stock.sh
@@ -13,7 +13,7 @@ cp drivers/crypto/msm/qce40.ko build_dir/system/lib/modules
 cp drivers/crypto/msm/qcedev.ko build_dir/system/lib/modules
 cp drivers/crypto/msm/qcrypto.ko build_dir/system/lib/modules
 cp drivers/input/evbug.ko build_dir/system/lib/modules
-cp drivers/interceptor/vpnclient.ko build_dir/system/lib/modules
+#cp drivers/interceptor/vpnclient.ko build_dir/system/lib/modules
 cp drivers/media/video/gspca/gspca_main.ko build_dir/system/lib/modules
 cp drivers/net/wireless/bcmdhd/dhd.ko build_dir/system/lib/modules
 cp drivers/net/wireless/btlock/btlock.ko build_dir/system/lib/modules
@@ -21,7 +21,7 @@ cp drivers/scsi/scsi_wait_scan.ko build_dir/system/lib/modules
 cp drivers/spi/spidev.ko build_dir/system/lib/modules
 
 ../packaging/mkbootfs ramdisk/initramfs | gzip > ramdisk/ramdisk.gz
-../packaging/mkbootimg --kernel arch/arm/boot/zImage --ramdisk ramdisk/ramdisk.gz --cmdline "androidboot.hardware=qcom user_debug=31" -o build_dir/boot.img --base 0x80200000 --pagesize 2048 --ramdiskaddr 0x81500000
+../packaging/mkbootimg --kernel arch/arm/boot/zImage --ramdisk ramdisk/ramdisk.gz --cmdline "androidboot.hardware=qcom user_debug=31 no_console_suspend" -o build_dir/boot.img --base 0x80200000 --pagesize 2048 --ramdiskaddr 0x81500000
 
 cd build_dir
 
