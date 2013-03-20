06ca9f29a81a65c95936444eb5c1075f5febc804
Steve Loebrich <sloebric@gmail.com>
2013-03-20 09:58:58 -0700

    BUILD: Updates to build boot.img into the flashable

--- build.sh
+++ build.sh
@@ -21,15 +21,20 @@ cp drivers/net/wireless/bcmdhd/dhd.ko build_dir/Boot-CM10.1/realsystem/lib/modul
 cp drivers/net/wireless/btlock/btlock.ko build_dir/Boot-CM10.1/realsystem/lib/modules
 cp drivers/scsi/scsi_wait_scan.ko build_dir/Boot-CM10.1/realsystem/lib/modules
 cp drivers/spi/spidev.ko build_dir/Boot-CM10.1/realsystem/lib/modules
+cp drivers/char/adsprpc.ko build_dir/Boot-CM10.1/realsystem/lib/modules
+cp block/test-iosched.ko build_dir/Boot-CM10.1/realsystem/lib/modules
+cp drivers/hid/hid-logitech-dj.ko build_dir/Boot-CM10.1/realsystem/lib/modules
 
 cd ramdisk/initramfs
-find . | cpio -o -H newc | gzip > ../../build_dir/Boot-CM10.1/boot.img-ramdisk.gz 
+find . | cpio -o -H newc | gzip > ../../build_dir/boot.img-ramdisk.gz
 
 cd ../..
 
-cp arch/arm/boot/zImage build_dir/Boot-CM10.1
+#cp arch/arm/boot/zImage build_dir/Boot-CM10.1
 
 cd build_dir/Boot-CM10.1
+
+../../../packaging/mkbootimg.new --kernel ../../arch/arm/boot/zImage --ramdisk ../boot.img-ramdisk.gz --ramdisk_offset 0x01300000 --base 0x80200000 --pagesize 2048 --board MSM8960 --cmdline "androidboot.hardware=qcom user_debug=31 zcache" --output boot.img 
 
 zip -r ~/Google\ Drive/I747\ Kernels/CM10.1/iX-CM10.1-I747-Kernel-"$timestamp".zip *
 
