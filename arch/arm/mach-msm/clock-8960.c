6b752e76997f32a4dcf53a1977ce54d6fe4b4161
InstigatorX <sloebric@gmail.com>
2013-05-11 11:23:35 -0700

    GPU: OC to 500MHz

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -3552,6 +3552,7 @@ static struct clk_freq_tbl clk_tbl_gfx3d_8960[] = {
 	F_GFX3D(320000000, pll2, 2,  5),
 	F_GFX3D(400000000, pll2, 1,  2),
 	F_GFX3D(450000000, pll3, 1,  2),
+	F_GFX3D(500000000, pll3, 1,  2),
 	F_END
 };
 
@@ -3646,7 +3647,7 @@ static struct rcg_clk gfx3d_clk = {
 		.dbg_name = "gfx3d_clk",
 		.ops = &clk_ops_rcg,
 		VDD_DIG_FMAX_MAP3(LOW,  128000000, NOMINAL, 300000000,
-				  HIGH, 450000000),
+				  HIGH, 500000000),
 		CLK_INIT(gfx3d_clk.c),
 		.depends = &gmem_axi_clk.c,
 	},
@@ -3923,7 +3924,7 @@ static struct rcg_clk mdp_clk = {
 	.c = {
 		.dbg_name = "mdp_clk",
 		.ops = &clk_ops_rcg,
-		VDD_DIG_FMAX_MAP2(LOW, 96000000, NOMINAL, 200000000),
+		VDD_DIG_FMAX_MAP2(LOW, 96000000, NOMINAL, 266667000),
 		CLK_INIT(mdp_clk.c),
 		.depends = &mdp_axi_clk.c,
 	},
--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -3559,19 +3559,24 @@ static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
+			.gpu_freq = 500000000,
+			.bus_freq = 4,
+			.io_fraction = 0,
+		},
+		{
 			.gpu_freq = 450000000,
 			.bus_freq = 4,
 			.io_fraction = 0,
 		},
 		{
 			.gpu_freq = 400000000,
-			.bus_freq = 4,
-			.io_fraction = 0,
+			.bus_freq = 3,
+			.io_fraction = 33,
 		},
 		{
 			.gpu_freq = 300000000,
 			.bus_freq = 3,
-			.io_fraction = 33,
+			.io_fraction = 66,
 		},
 		{
 			.gpu_freq = 200000000,
@@ -3588,8 +3593,8 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.bus_freq = 0,
 		},
 	},
-	.init_level = 1,
-	.num_levels = ARRAY_SIZE(grp3d_freq) + 1,
+	.init_level = 3,
+	.num_levels = 7,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
 	.nap_allowed = true,
