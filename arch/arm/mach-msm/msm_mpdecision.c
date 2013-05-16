aec5dde123faf25ed22b87754538c8a7ef80f22a
InstigatorX <sloebric@gmail.com>
2013-05-16 14:25:06 -0700

    MPDEC: Tuning for more aggressive battery savings

--- arch/arm/mach-msm/msm_mpdecision.c
+++ arch/arm/mach-msm/msm_mpdecision.c
@@ -128,8 +128,8 @@ static struct msm_mpdec_tuners {
 #endif
 };
 
-static unsigned int NwNs_Threshold[8] = {12, 0, 20, 7, 25, 10, 0, 18};
-static unsigned int TwTs_Threshold[8] = {140, 0, 140, 190, 140, 190, 0, 190};
+static unsigned int NwNs_Threshold[8] = {30, 0, 20, 15, 25, 10, 0, 18};
+static unsigned int TwTs_Threshold[8] = {140, 0, 140, 250, 140, 190, 0, 190};
 
 extern unsigned int get_rq_info(void);
 extern unsigned long acpuclk_get_rate(int);
